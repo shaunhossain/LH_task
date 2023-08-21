1f2e3c7471a1729b1c745bae9ae7f8dba6847720
shaunhossain <shaunhossain95@gmail.com>
2023-08-21 17:30:27 +0600

    add sponsor ui

--- lib/ui/page/sponsor_page/sponsor_page.dart
+++ lib/ui/page/sponsor_page/sponsor_page.dart
@@ -9,7 +9,11 @@ class SponsorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
-    return SafeArea(child: Scaffold(
+    List<String> widgetList = ['A', 'B', 'C'];
+    List<String> widgetList2 = ['A', 'B', 'C','D','E','F','G'];
+
+    return SafeArea(
+        child: Scaffold(
       backgroundColor: Colors.white,
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16.0),
@@ -19,13 +23,68 @@ class SponsorPage extends StatelessWidget {
               elevation: 0,
               color: darkWhiteColor,
               shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(8)
-              ),
-              child: const Padding(
-                padding: EdgeInsets.all(16.0),
+                  borderRadius: BorderRadius.circular(8)),
+              child: Container(
+                width: SizeConfig.width,
+                height: widgetList.length > 4 ? 200 : 160,
+                padding: const EdgeInsets.all(16.0),
                 child: Column(
                   children: [
-                    Text("data"),
+                    const Text("data"),
+                    Flexible(
+                      child: GridView.count(
+                          crossAxisCount: 2,
+                        childAspectRatio: 2.5,
+                        children: widgetList.map((String value) {
+                          return Container(
+                            height: 20.0,
+                            color: Colors.green,
+                            margin: const EdgeInsets.all(10.0),
+                            child: Center(
+                              child: Text(
+                                value,
+                                style: const TextStyle(fontSize: 18.0,color: Colors.white),
+                              ),
+                            ),
+                          );
+                        }).toList(),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Card(
+              elevation: 0,
+              color: darkWhiteColor,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8)),
+              child: Container(
+                width: SizeConfig.width,
+                height: widgetList2.length > 4 ? 220 : 160,
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    const Text("data"),
+                    Flexible(
+                      child: GridView.count(
+                        crossAxisCount: 2,
+                        childAspectRatio: 2.5,
+                        children: widgetList2.map((String value) {
+                          return Container(
+                            height: 20.0,
+                            color: Colors.green,
+                            margin: const EdgeInsets.all(10.0),
+                            child: Center(
+                              child: Text(
+                                value,
+                                style: const TextStyle(fontSize: 18.0,color: Colors.white),
+                              ),
+                            ),
+                          );
+                        }).toList(),
+                      ),
+                    )
                   ],
                 ),
               ),
