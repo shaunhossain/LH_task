67117f7210836ed933dede41d68c82d4ea89118a
shaun hossain <shaunhossain95@gmail.com>
2023-08-25 18:23:22 +0600

    improve home page timeline view

--- lib/ui/page/home_page/home_page.dart
+++ lib/ui/page/home_page/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
@@ -12,7 +14,6 @@ import 'package:react_conf/core/util/size_config.dart';
 import 'package:react_conf/core/util/styles.dart';
 import 'package:react_conf/ui/widgets/custom_retry_button.dart';
 import 'package:react_conf/ui/widgets/home_page/custom_conference_item.dart';
-import 'package:react_conf/ui/widgets/home_page/custom_conference_last_item.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -39,112 +40,129 @@ class _HomePageState extends State<HomePage> {
     SizeConfig().init(context);
     return SafeArea(
         child: Scaffold(
-      backgroundColor: whiteColor,
-      appBar: AppBar(
-        title: Padding(
-          padding: const EdgeInsets.only(top: 16.0, bottom: 8),
-          child: SvgPicture.asset(
-            "assets/react_conf_icon.svg",
-            width: SizeConfig.width! * 0.29,
-            height: SizeConfig.height! * 0.06,
-            fit: BoxFit.cover,
+          backgroundColor: whiteColor,
+          appBar: AppBar(
+            title: Padding(
+              padding: const EdgeInsets.only(top: 16.0, bottom: 8),
+              child: SvgPicture.asset(
+                "assets/react_conf_icon.svg",
+                width: SizeConfig.width! * 0.29,
+                height: SizeConfig.height! * 0.06,
+                fit: BoxFit.cover,
+              ),
+            ),
+            centerTitle: true,
+            shadowColor: shadowColor,
+            elevation: 8,
           ),
-        ),
-        centerTitle: true,
-        shadowColor: shadowColor,
-        elevation: 8,
-      ),
-      body:
+          body:
           BlocListener<InternetBloc, InternetState>(listener: (context, state) {
-        if (state is Disconnected) {
-          setState(() {
-            hasInternet = false;
-          });
-        } else {
-          setState(() {
-            hasInternet = true;
-          });
-        }
-      }, child: BlocBuilder<ConferenceListBloc, ConferenceRequestState>(
-        builder: (context, state) {
-          if (state is SendingConferenceRequest) {
-            return const Center(
-              child: CircularProgressIndicator(
-                color: primaryColor,
-              ),
-            );
-          }
-          if (state is GetConferenceListSuccessfully) {
-            return RefreshIndicator(
-              onRefresh: () async {
-                _getConferenceList();
-              },
-              child: CustomScrollView(
-                slivers: [
-                  const SliverToBoxAdapter(
-                    child: SizedBox(
-                      height: 32,
-                    ),
+            if (state is Disconnected) {
+              setState(() {
+                hasInternet = false;
+              });
+            } else {
+              setState(() {
+                hasInternet = true;
+              });
+            }
+          }, child: BlocBuilder<ConferenceListBloc, ConferenceRequestState>(
+            builder: (context, state) {
+              if (state is SendingConferenceRequest) {
+                return const Center(
+                  child: CircularProgressIndicator(
+                    color: primaryColor,
                   ),
-                  SliverList.builder(
-                      itemCount: state.listOfConference.length,
-                      itemBuilder: (context, index) {
-                        if (state.listOfConference.length == index + 1) {
-                          return CustomConferenceLastItem(
-                            title: state.listOfConference[index].name ?? "",
-                            about: state.listOfConference[index].slogan ?? "",
-                            date: DateFormat('d MMMM, yyyy').format(
-                                state.listOfConference[index].startDate!),
-                            onTap: () {
-                              Navigator.of(context).pushNamed(
-                                  '/conference-info',
-                                  arguments: state.listOfConference[index].id
+                );
+              }
+              if (state is GetConferenceListSuccessfully) {
+                return RefreshIndicator(
+                  onRefresh: () async {
+                    _getConferenceList();
+                  },
+                  child: CustomScrollView(
+                    slivers: [
+                      const SliverToBoxAdapter(
+                        child: SizedBox(
+                          height: 32,
+                        ),
+                      ),
+                      SliverPadding(
+                        padding: const EdgeInsets.symmetric(horizontal: 16),
+                        sliver: SliverList.builder(
+                            itemCount: state.listOfConference.length,
+                            itemBuilder: (context, index) {
+                              if (index == 0) {
+                                return CustomConferenceItem(
+                                  title: state.listOfConference[index].name ?? "",
+                                  about: state.listOfConference[index].slogan ?? "",
+                                  date: DateFormat('d MMMM, yyyy')
+                                      .format(state.listOfConference[index].startDate!),
+                                  onTap: () {
+                                    Navigator.of(context).pushNamed(
+                                        '/conference-info',
+                                        arguments: state.listOfConference[index].id
+                                    );
+                                  }, topLineColor: whiteColor, bottomLineColor: primaryColor,
+                                );
+                              }
+                              if (state.listOfConference.length == index + 1) {
+                                return CustomConferenceItem(
+                                  title: state.listOfConference[index].name ?? "",
+                                  about: state.listOfConference[index].slogan ?? "",
+                                  date: DateFormat('d MMMM, yyyy')
+                                      .format(state.listOfConference[index].startDate!),
+                                  onTap: () {
+                                    Navigator.of(context).pushNamed(
+                                        '/conference-info',
+                                        arguments: state.listOfConference[index].id
+                                    );
+                                  }, topLineColor: primaryColor, bottomLineColor: whiteColor,
+                                );
+                              }
+                              return CustomConferenceItem(
+                                title: state.listOfConference[index].name ?? "",
+                                about: state.listOfConference[index].slogan ?? "",
+                                date: DateFormat('d MMMM, yyyy')
+                                    .format(state.listOfConference[index].startDate!),
+                                onTap: () {
+                                  Navigator.of(context).pushNamed(
+                                      '/conference-info',
+                                      arguments: state.listOfConference[index].id
+                                  );
+                                }, topLineColor: primaryColor, bottomLineColor: primaryColor,
                               );
-                            },
-                          );
-                        }
-                        return CustomConferenceItem(
-                          title: state.listOfConference[index].name ?? "",
-                          about: state.listOfConference[index].slogan ?? "",
-                          date: DateFormat('d MMMM, yyyy')
-                              .format(state.listOfConference[index].startDate!),
-                          onTap: () {
-                            Navigator.of(context).pushNamed(
-                              '/conference-info',
-                                arguments: state.listOfConference[index].id
-                            );
-                          },
-                        );
-                      })
-                ],
-              ),
-            );
-          }
+                            }),
+                      )
+                    ],
+                  ),
+                );
+              }
 
-          return Center(
-              child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: SizeConfig.height! * 0.3,
-              ),
-              Text(
-                'No Data Found',
-                style: h4TextStyle(darkGreyColor),
-              ),
-              SizedBox(
-                  width: SizeConfig.width! * 0.3,
-                  child: CustomRetryButton(
-                      title: "Retry",
-                      color: primaryColor,
-                      onPress: () {
-                        _getConferenceList();
-                      },
-                      textColor: Colors.white))
-            ],
-          ));
-        },
-      )),
-    ));
+              return Center(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        height: SizeConfig.height! * 0.3,
+                      ),
+                      Text(
+                        'No Data Found',
+                        style: h4TextStyle(darkGreyColor),
+                      ),
+                      SizedBox(
+                          width: SizeConfig.width! * 0.3,
+                          child: CustomRetryButton(
+                              title: "Retry",
+                              color: primaryColor,
+                              onPress: () {
+                                _getConferenceList();
+                              },
+                              textColor: Colors.white))
+                    ],
+                  ));
+            },
+          )),
+        ));
   }
 }
